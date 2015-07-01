96c4b8b6b3bfd3eaa62377b4dc3fec2987e3c07e
Max Zhou <ball908765@gmail.com>
2015-06-30 22:49:33 -0400

    uh got a page working..?

---
+++ pages/about.js
@@ -0,0 +1,17 @@
+orion.pages.addTemplate(
+    {
+        template: 'about',
+        name: 'contact',
+        layout: 'layout',
+        description: 'About template(??)'
+    },
+    {
+        content: orion.attribute('froala', {
+            label: 'content'
+        })
+    }
+);
+
+
+
+
